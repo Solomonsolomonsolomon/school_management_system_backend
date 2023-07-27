560579f309c2e00b228b341a82217adc122a4fcf
Solomon Emmanuel <soulex04292003@gmail.com>
2023-07-27 01:02:39 +0100

    added admin  delete users

--- server/controller/admin.controller.ts
+++ server/controller/admin.controller.ts
@@ -98,7 +98,7 @@ export async function addStudent(
     await Student.findOne({ name, email }).then(
       async (alreadyRegistered: object | null) => {
         if (alreadyRegistered) {
-          throw new Error("This particular teacher already registered");
+          throw new Error("This particular student already registered");
         } else {
           await new Student({
             email,
@@ -125,6 +125,99 @@ export async function addStudent(
       error,
       err: error.message,
       msg: "failed to add student",
+    });
+  }
+}
+
+export async function deleteAdmin(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const { id } = req.params;
+    await Admin.findOneAndRemove({ _id: id })
+      .then((user) => {
+        if (!user) {
+          throw new Error("Invalid Admin Id");
+        }
+        res.status(200).json({
+          status: 200,
+          msg: "deleted admin successfully",
+          user,
+        });
+      })
+      .catch((err) => {
+        throw err;
+      });
+  } catch (error: any) {
+    res.status(400).json({
+      status: 400,
+      msg: "failed to delete user",
+      err: error.message,
+      error,
+    });
+  }
+}
+
+export async function deleteStudent(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const { studentId } = req.params;
+    await Student.findOneAndRemove({ studentId })
+      .then((user) => {
+        if (!user) {
+          throw new Error("Invalid studentId");
+        }
+        res.status(200).json({
+          status: 200,
+          msg: "deleted student successfully",
+          user,
+        });
+      })
+      .catch((err) => {
+        throw err;
+      });
+  } catch (error: any) {
+    res.status(400).json({
+      status: 400,
+      msg: "failed to delete user",
+      err: error.message,
+      error,
+    });
+  }
+}
+
+export async function deleteTeacher(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const { teacherId } = req.params;
+    await Teacher.findOneAndRemove({ teacherId })
+      .then((user) => {
+        if (!user) {
+          throw new Error("Invalid teacherId");
+        }
+        res.status(200).json({
+          status: 200,
+          msg: "deleted teacher successfully",
+          user,
+        });
+      })
+      .catch((err) => {
+        throw err;
+      });
+  } catch (error: any) {
+    res.status(400).json({
+      status: 400,
+      msg: "failed to delete user",
+      err: error.message,
+      error,
     });
   }
 }
