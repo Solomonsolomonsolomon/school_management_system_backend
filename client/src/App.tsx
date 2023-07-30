50a3c2217eb62d58494db71706cd772bf61ffd08
Baskinsen <92514412+Baskinsen@users.noreply.github.com>
2023-07-31 00:25:14 +0100

    Revert "login authentication completed"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,24 @@
-import { useState, useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
+// import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Admin from "./components/Admin";
-import AdminStudent from "./components/Admin/Students"
-import AdminTeacher from "./components/Admin/Teachers"
-import Student from "./components/Student/Student";
-import Teacher from "./components/Teacher/Teacher";
-import RequireAuth from "./Auth/RequireAuth";
-import "./index.css"; 
- 
+import { AuthProvider } from "./context/AuthProvider";
+import "./index.css";
+
 function App() {
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          {/* public routes */ }
-        <Route path="login" element={<Login />} />
-        {/* <Route path="forgotPassword" element={<Login />} /> */}
+      <AuthProvider>
+        <BrowserRouter>
+          <div className="App">
+            <Routes>
+                <Route  path="/" element={<Login/>} />
+                <Route path="/Admin" element={<Admin/>} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </AuthProvider>
 
-        {/* protected routes */ }
-        <Route element={<RequireAuth />}>
-          <Route path="/admin" element={<Admin />} />
-          <Route path="admin/student" element={<AdminStudent />} />
-          <Route path="admin/teacher" element={<AdminTeacher />} />
-          <Route path="student" element={<Student />} />
-          <Route path="teacher" element={<Teacher />} />
-        </Route>
-
-
-        {/* catch all */ }
-        <Route path="*" element={<h1>Not Found</h1>} />
-        </Route>
-
-      </Routes>
     </>
   );
 }
--- client/src/components/Admin.tsx
+++ client/src/components/Admin.tsx
@@ -1,53 +1,17 @@
 import axios from '../api/axios'
-import React, {useContext, useEffect, useRef, useState} from 'react'
+import React, {useContext, useRef, useState} from 'react'
 import AuthContext from '../context/AuthProvider'
 import { Link } from 'react-router-dom';
-import Navbar from './Navbar'
 
- const  Admin = ()=> {
-    // useEffect(() => {
-
-    //     // axios.get('/admin')
-    //     // .then(res => {
-    //     //     console.log(res.data)
-    //     // })
-    //     // .catch(err => {
-    //     //     console.log(err.response)
-    //     // })
-    // }, [])
-
-    const logout=()=> {
-        localStorage.removeItem('accessToken')
-        localStorage.removeItem('user')
-        window.location.reload()
-    }
-
-
+ export default Admin = ()=> {
     return (
         <div className='container grid  '>
-        <Navbar />
-        <main className='container '>
-            <section>
-                <p>Admin</p>
-                <button onClick={logout}>log out</button>
-            </section>
-            <section>
 
-            </section>
-            <section>
-
-            </section>
-            <section>
-
-            </section>
-            <section>
-
-            </section>
+        <main>
+            <h1>Admin</h1>
         </main>
         </div>
         
 
     ) 
 }
-
-export default Admin
